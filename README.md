87e0229b65bb15840d7242f52e799e9b0a332c76
williamjackson314 <wtjackson314@gmail.com>
2021-10-05 23:09:06 -0400

    Add support functions and also add function descriptions to README.

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
-**Binary Tree**
+**Required Libraries**
+- csv : to read in the csv file with the data to binary tree-ize
+- math : to find the number of levels of the tree from the length of the list
 
+**Suport Functions**
+- read_file : read data from csv file, convert to list of integers, return this list
+- add_level : Add a level of None type elements to the tree. Used when the index to insert a new element is out of
+                the range of the binary search tree list
 
-Implement Binary Tree data structure, with Insert, Remove, Search, Minimum, and Maximum functions supported.
+**Binary Search Tree**
 
-Insert :
-- Insert specified item into the binary tree in the proper place.
+Implement Binary Search Tree data structure using an list. The functions Insert, Search, Minimum, 
+and Maximum are supported.
 
-Remove:
-- Remove the specified item from the binary tree and reconstruct tree without removed element.
+Functions
+    Insert :
+    - Insert specified item into the binary tree in the proper place.
 
-Search:
-- Search through the binary tree and return the location of the target item.
+    Search :
+    - Search through the binary tree and return the index of the target item.
 
-Minimum:
-- Find the smallest valued item in the binary tree.
+    Minimum :
+    - Find the smallest value item in the binary tree.
 
-Maximum:
-- Find the largest value item in the binary tree.
+    Maximum :
+    - Find the largest value item in the binary tree.
+
+    Main :
+    - Read in csv data
+    - Create the tree list with one None type element as the root
+    - Iterate through the csv data and call Insert to insert each data point into the tree
--- Trees.py
+++ Trees.py
@@ -1,53 +1,74 @@
-def set_root(tree, data) :
-    if tree[0] is None :
-        tree[0] = data
-    else :
-        return 0
+import csv
+import math
 
+''' Support Functions '''    
+def read_file(file_name) :
+
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        data = list(reader)
+
+    data = [int(x[0]) for x in data[:]]
+    return data
+
+def add_level(tree) :
+    levels = math.floor(math.log(len(tree), 2) + 1)
+    for num in range(0, 2**levels) :
+        tree.append(None)
+
+''' Tree Functions '''
 def insert(tree, data) :
-    return 0
+    i = 0
+    while tree[i] is not None :
+        if data <= tree[i] :
+            i = (2 * i) + 1
+        elif data > tree[i] :
+            i = (2 * i) + 2 
+
+        if i >= len(tree) :
+            add_level(tree)
+
+    tree[i] = data
 
 def search(tree, target):
     i = 0
-    while i < len(tree) :
+    while i < len(tree) and tree[i] is not None :
         if target == tree[i] :
             return i
         elif target < tree[i] :
             i = (2 * i) + 1
-        else : # target > trees[i]
-            i = (2 * i) + 2
-    
-    return None
+        else : # target > tree[i]
+            i = (2 * i) + 2 
 
-def remove(tree, data) :
-    i = search(tree, data)
-    '''
-    while i < len(tree) - 1 :
-        tree[i] = tree[i + 1]
-        i += 1
-    tree.pop()
-    '''
-    print(tree)
+    return i
 
 def maximum(tree) :
     i = 0
-    while i < len(tree) and tree[i] is not None :
+    while tree[i] is not None :
         j = i
         i = (2 * i) + 2
-    
-    return j
+
+        if i >= len(tree) :
+            add_level(tree)
+
+    return tree[j]
 
 def minimum(tree) :
     i = 0
-    while i < len(tree) and tree[i] is not None:
+    while tree[i] is not None:
         j = i
         i = (2 * i) + 1
-    
-    return j
+
+        if i >= len(tree) :
+            add_level(tree)        
+
+    return tree[j]
 
 def main() :
-    tree = [13, None, 16, None, None, 14, 18]
-    print(str(minimum(tree)))
+    data = read_file("test1.csv")
+    tree = [None]
+    for num in data[:] :
+        insert(tree, num)
     return 0
 
 main()
