d63aeba8dca575d61b7797f8b9e8f2718c584edb
williamjackson314 <wtjackson314@gmail.com>
2021-10-27 21:24:42 -0400

    Implement UI and finish writing README.

--- README.md
+++ README.md
@@ -1,32 +1,67 @@
 **Required Libraries**
-- csv : to read in the csv file with the data to binary tree-ize
-- math : to find the number of levels of the tree from the length of the list
+- csv : to read in the csv file with the data to binary tree-ize.
+- math : to find the number of levels of the tree from the length of the list.
 
 **Support Functions**
-- read_file : read data from csv file, convert to list of integers, return this list
-- add_level : Add a level of None type elements to the tree. Used when the index to insert a new element is out of
-                the range of the binary search tree list
+- read_file : read data from csv file, convert to list of integers, return this list.
+- add_level : Add a level of None type elements to the tree. Used when the index to insert a new element is out of the range of the binary search tree list.
+- swap : swap two items in a list.
+- left : find the index of the left child of the current node.
+- right : find the index of the right child of the current node.
 
-**Binary Search Tree**
-
-Implement Binary Search Tree data structure using an list. The functions Insert, Search, Minimum, 
+**Binary Search Tree (BST)**
+- Binary Search Tree property states that a current node will be larger than its left child and smaller than
+its right child
+- Constructs a list that follows the Binary Search Tree property using data from csv. The functions Insert, Search, Minimum,
 and Maximum are supported.
 
 **Tree Functions** :
 
-Insert :
-- Insert specified item into the binary tree in the proper place.
+insert :
+- Insert the specified item into the binary tree in the proper place.
+- When an item already in the tree is passed to insert, the function places it in the left subtree of the node it equals.
+- Uses the add_level() support function when inserting into a new level.
 
-Search :
+search :
 - Search through the binary tree and return the index of the target item.
 
-Minimum :
+minimum :
 - Find the smallest value item in the binary tree.
+- Does this by iteratively calling the left() support function until a 'None' valued child is found.
 
-Maximum :
+maximum :
 - Find the largest value item in the binary tree.
+- Does this by iteratively calling the right() support function until a 'None' valued child is found.
 
-Main :
-- Read in csv data
-- Create the tree list with one None type element as the root
-- Iterate through the csv data and call Insert to insert each data point into the tree
+**Traversals** :
+inorder :
+- Prints out nodes in left, middle, right order.
+
+preorder :
+- Prints out nodes in middle, left, right order.
+
+postorder :
+- Prints out nodes in left, right, middle order.
+
+
+**Max Heaps**
+- The Max Heap property states that the current node will be larger than its left and right children
+- Constructs a list that follows the Max Heap property using data from a csv. Max Heapify and Build Max Heap functions are supported.
+
+**Max Heap Functions** :
+
+max_heapify :
+- Make the current node follow the max heap property.
+- If current node not larger than both its children, swap it with its largest child
+
+build_max_heap :
+- Construct a Max Heap from a list
+- Iteratively calls max_heapify to rearrange list to follow the Max Heap property
+
+**Main**
+main :
+- Read in csv data.
+- Read in data from the csv file requested by the user
+- Ask the user if they want to construct a BST, print BST, and print out specific values for the BST
+- Ask the user if they want to construct a Max Heap, print Max Heap
+- Repeat until they say no when asked to construct a new BST and/or Max Heap
--- Trees.py
+++ Trees.py
@@ -16,14 +16,25 @@ def add_level(tree) :
     for num in range(0, 2**levels) :
         tree.append(None)
 
+def swap(tree, index1, index2) :
+    temp = tree[index1]
+    tree[index1] = tree[index2]
+    tree[index2] = temp
+
+def left(index) :
+    return (2 * index + 1)
+
+def right(index):
+    return (2 * index + 2)
+
 ''' Tree Functions '''
 def insert(tree, data) :
     i = 0
     while tree[i] is not None :
         if data <= tree[i] :
-            i = (2 * i) + 1
+            i = left(i)
         elif data > tree[i] :
-            i = (2 * i) + 2 
+            i = right(i)
 
         if i >= len(tree) :
             add_level(tree)
@@ -36,28 +47,29 @@ def search(tree, target):
         if target == tree[i] :
             return i
         elif target < tree[i] :
-            i = (2 * i) + 1
+            i = left(i)
         else : # target > tree[i]
-            i = (2 * i) + 2 
+            i = right(i) 
 
     return i
 
-def maximum(tree) :
+def minimum(tree) :
     i = 0
-    while tree[i] is not None :
+    while tree[i] is not None:
         j = i
-        i = (2 * i) + 2
+        i = left(i)
 
         if i >= len(tree) :
             add_level(tree)
 
     return tree[j]
 
-def minimum(tree) :
+
+def maximum(tree) :
     i = 0
-    while tree[i] is not None:
+    while tree[i] is not None :
         j = i
-        i = (2 * i) + 1
+        i = right(i)
 
         if i >= len(tree) :
             add_level(tree)
@@ -65,30 +77,99 @@ def minimum(tree) :
     return tree[j]
 
 def inorder(tree, node_index) :
-    if tree[node_index] is not None :
-        inorder(tree, node_index * 2 + 1)
+    if node_index < len(tree) and tree[node_index] is not None :
+        inorder(tree, left(node_index))
         print(tree[node_index])
-        inorder(tree, node_index * 2 + 2)
+        inorder(tree, right(node_index))
 
 def preorder(tree, node_index) :
-    if tree[node_index] is not None :
+    if node_index < len(tree) and tree[node_index] is not None :
         print(tree[node_index])
-        preorder(tree, node_index * 2 + 1)
-        preorder(tree, node_index * 2 + 2)
+        preorder(tree, left(node_index))
+        preorder(tree, right(node_index))
 
 def postorder(tree, node_index) :
-    if tree[node_index] is not None :
-        postorder(tree, node_index * 2 + 1)
-        postorder(tree, node_index * 2 + 2)
+    if node_index < len(tree) and tree[node_index] is not None :
+        postorder(tree, left(node_index))
+        postorder(tree, right(node_index))
         print(tree[node_index])
 
+def max_heapify(tree, index) :
+
+    key = tree[index]
+
+    if tree[left] is not None and tree[left] > key :
+        largest = left(index)
+    else :
+        largest = index
+    if tree[right] is not None and tree[right] > tree[largest] :
+        largest = right(index)
+    if largest != index :
+        swap(tree, index, largest)
+        max_heapify(tree, largest)
+
+def build_max_heap(tree) :
+    i = (len(tree) // 2) - 1
+    while tree[i] is not None :
+        max_heapify(tree, i)
+        i -= 1
 
 
 def main() :
-    data = read_file("test1.csv")
-    tree = [50,25,75,10,40,60,90, None, None, None, None, None, None, None, None]
-    # for num in data[:] :
-    #     insert(tree, num)
-    return 0
+    selection = input("Do you want to construct a new BST and/or Max Heap? [y/n] ")
+    while selection == "y" :
+
+        files = ["test1.csv", "test2.csv", "test3.csv"]
+        tree = [None]
+
+        file_choice = input("Please select test file to use: 1, 2, or 3 ")
+        while (file_choice != "1") and (file_choice != "2") and (file_choice != "3") :
+            print("Invalid choice\n")
+            file_choice = input("Please select test file to use: 1, 2, or 3\n")
+
+        data = read_file(files[int(file_choice) - 1])
+        for num in data[:] :
+            insert(tree, num)
+
+        print_choice = input("Do you want to print the BST? [y/n] ")
+        if print_choice == "y" :
+            print(tree)
+
+        print_choice = input("Do you want to print the max and min values? [y/n] ")
+        if print_choice == "y" :
+            tree_max = maximum(tree)
+            tree_min = minimum(tree)
+            print("Tree Max = " + str(tree_max) + "\n")
+            print("Tree Min = " + str(tree_min) + "\n")
+
+        user_search = input("Enter item to search: ('q' to not search) ")
+        if user_search != "q" :
+            item_index = search(tree, int(user_search))
+            print("Item " + user_search + " at index " + str(item_index) + "\n")
+
+        choice = input("Print traversals? [y/n] ")
+        if choice == "y" :
+            print("Inorder Traversal: ")
+            inorder(tree, 0)
+            print('\n')
+
+            print("Preorder Traversal: ")
+            preorder(tree, 0)
+            print('\n')
+
+            print("Postorder Traversal: ")
+            postorder(tree, 0)
+            print('\n')
+
+        heap_choice = input("Do you want to construct a Max Heap? [y/n] ")
+        if heap_choice == "y" :
+            heap = tree
+            heap = build_max_heap(heap)
+
+            print_choice = input("Do you want to print the Max Heap? [y/n] ")
+            if print_choice == "y" :
+                print(heap)
+
+        selection = input("Do you want to construct a new BST and/or Max Heap? [y/n] ")
 
 main()
